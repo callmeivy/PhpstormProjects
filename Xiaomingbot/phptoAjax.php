a7cdc3194aad88e6151074ae3694cbe805ca8557
ivy <c.ivy.jin@foxmail.com>
2018-08-10 17:48:13 +0800

    none

---
+++ Xiaomingbot/phptoAjax.php
@@ -0,0 +1,15 @@
+<?php
+error_reporting(0);  //禁用报错信息
+require_once("conn.php");//导入公共连接数据库文件
+//echo "connecting";
+$query="select * from tdt_archives";
+$qry_result=$mysqli->query($query);//这里设置以倒序的方法读取十组数据
+$jsonArray=array();//新建数据用于接收数据库每行对应的数据组
+while($rows=mysqli_fetch_object($qry_result))
+{
+    array_push($jsonArray,$rows);     //把数据库的内容添加到新建数组中
+
+}
+echo json_encode($jsonArray); //转换成json传递给前端
+$mysqli->close();
+?>
---
+++ Xiaomingbot/switch.js
@@ -0,0 +1,8 @@
+window['toutiao.web.toutiao_pc/pc/fe_switch'] = {
+    "qihuAdShow": true,
+    "ttAdShow": true,
+    "picVersion": "20180412_01",
+    "migScriptUrl": "img.min.js",
+    "adScriptQihu": true,
+    "adScriptTB": true
+}
